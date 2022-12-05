9ecfe528fb69ee9372be8ac0cc82975ff7b6b58c
Haron098 <alkeevvalba@gmail.com>
2022-12-05 17:38:06 +0600

    book

--- applications/account/urls.py
+++ applications/account/urls.py
@@ -9,15 +9,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 from applications.account.views import *
-#
-# urlpatterns = [
-#     path('register/', RegisterAPIView.as_view()),
-#     path('send_mail/', send_hello_api_view),
-#     path('activate/<uuid:activation_code>/', ActivationApiView.as_view()),
-#     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-#     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-#     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify')
-# ]
+
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view()),
